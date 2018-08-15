69d22818822b1739cfc1f75fbd6394fb3ff1aa7e
Kenneth Seals-Nutt <k.sealsnutt@gmail.com>
2018-08-14 22:28:44 -0700

    FEAT: Revamp of homepage

--- static/js/home.js
+++ static/js/home.js
@@ -1,76 +1,79 @@
-function loadFeatureStories(){
-  var $cont = document.querySelector('.cont');
-  var $elsArr = [].slice.call(document.querySelectorAll('.el'));
-  var $closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
-
-  setTimeout(function() {
-    $cont.classList.remove('s--inactive');
-  }, 200);
-
-  $elsArr.forEach(function($el) {
-    $el.addEventListener('click', function() {
-      if (this.classList.contains('s--active')) return;
-      $cont.classList.add('s--el-active');
-      this.classList.add('s--active');
-    });
-  });
-
-  $closeBtnsArr.forEach(function($btn) {
-    $btn.addEventListener('click', function(e) {
-      e.stopPropagation();
-      $cont.classList.remove('s--el-active');
-      document.querySelector('.el.s--active').classList.remove('s--active');
-    });
-  });
-
-}
-
-
-
-
-/*
- * Plugin intialization
- */
-
- // $.fn.pagepiling.setMouseWheelScrolling(false);
-$('#pagepiling').pagepiling({
-    sectionsColor: ['white', "#e1e8f0", '#535558'],
-    onLeave: function (index, nextIndex, direction) {
-
-        //fading out the txt of the leaving section
-        $('.section').eq(index - 1).find('h1, p').fadeOut(700, 'easeInQuart');
-
-        //fading in the text of the destination (in case it was fadedOut)
-        $('.section').eq(nextIndex - 1).find('h1, p').fadeIn(700, 'easeInQuart');
-
-
-        //reaching our last section? The one with our normal site?
-        if (nextIndex == 2) {
-            $('#arrow').hide();
-
-            //fading out navigation bullets
-            // $('#pp-nav').fadeOut();
-
-            $('#section2').find('.content').animate({
-                top: '0%'
-            }, 700, 'easeInQuart');
-            loadFeatureStories();
-        }
-
-        //leaving our last section? The one with our normal site?
-        if (index == 2) {
-            $('#arrow').show();
-
-            //fadding in navigation bullets
-            $('#pp-nav').fadeIn();
-
-            $('#section2 .content').animate({
-                top: '100%'
-            }, 700, 'easeInQuart');
-        }
+$('#featuredCarousel').slick({
+  centerMode: true,
+  arrows: false,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
     },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ],
 });
-$.fn.pagepiling.setAllowScrolling(false);
-$('#arrow').click(function () {
-    $.fn.pagepiling.moveSectionDown();
+
+
+$('#sourceCarousel').slick({
+  arrows: false,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1900,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerPadding: '40px',
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    }
+  ],
+});
+ScrollReveal({ distance: '100px' });
+ScrollReveal({ reset: true }).reveal('section',
+{
+  origin: 'bottom',
+  over: '3s'
+});
+ScrollReveal({ reset: true }).reveal('#features .left img',
+{ delay: 500 ,
+  origin: 'left',
+  over: '3s'
+});
+ScrollReveal({ reset: true }).reveal('#features .right img',
+{ delay: 500 ,
+  origin: 'right',
+  over: '3s'
+});
+ScrollReveal({ reset: true }).reveal('#features .bottom img',
+{ delay: 500 ,
+  origin: 'bottom',
+  over: '3s'
 });
