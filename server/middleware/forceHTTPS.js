0d216e2d77f4ada0780d2f957e6eba97ce1d639e
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-07-02 00:57:57 -0400

    (#137) CLEAN: responding to PR feedback, linter rules

--- server/middleware/forceHTTPS.js
+++ server/middleware/forceHTTPS.js
@@ -1,12 +1,12 @@
+const { FORCE_HTTPS } = require('../constants/index');
+
 function forceHTTPS(req, res, next) {
-
-    if (process.env.FORCE_HTTPS === 'true' && !req.secure) {
-       return res.redirect("https://" + req.headers.host + req.url);
-    }
-
+  if (FORCE_HTTPS && !req.secure) {
+    return res.redirect(`https://${req.headers.host}${req.url}`);
+  }
     next();
 }
 
 module.exports = {
-    forceHTTPS
-}
+    forceHTTPS,
+};
